9ee0e0a9cdfe95450a6bb44df53b1e87b9afdaca
AtharvKashyap <ak.atharvkashyap@gmail.com>
2025-08-04 21:58:42 +0000

    Finished home page UI

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { assets } from '../assets/frontend_assets/assets'
+
+const Footer = () => {
+  return (
+    <div>
+        <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-5 my-10 mt-10 text-sm'>
+            <div>
+                <img src={assets.logo} className='mb-5 w-32'/>
+                <p className='mr-50'> Thank you for shopping with us and being part of our journey.
+                </p>
+            </div>
+            <div>
+                <p className='text-xl font-medium mb-5'>COMPANY</p>
+                <ul className='flex flex-col gap-1 text-gray-600'>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Delivery</li>
+                    <li>Privacy Policy</li>
+                </ul>
+            </div>
+            <div>
+                <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+                <ul className='flex flex-col gap-1 text-gray-600'>
+                    <li>+221 123-456-7890</li>
+                    <li>IndiaMART@gmail.com</li>
+                </ul>
+            </div>
+        </div>
+
+        <div>
+            <hr />
+            <p className='py-5 text-sm text-center'>Copyright 2025 @IndiaMART.com all rights reserved.</p>
+        </div>
+
+    </div>
+  )
+}
+
+export default Footer
